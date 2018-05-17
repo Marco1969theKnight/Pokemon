120ec65fc82058c4d6532874d008f3bb628e2d02
Jaime Saúl <jasajsas@gmail.com>
2018-05-17 16:54:40 -0500

    Modificación clase Pokemon
    añadí ataques para cada pokemon.

--- pokemon.cc
+++ pokemon.cc
@@ -51,66 +51,118 @@ void Pokemon::setPokName(NomPok name)
         o_vida = 309;
         o_type = Type::Element::FIRE;
         o_name = "Charmander";
+        o_moves.push_back(Move::NameMove::FIRE_SPIN);
+        o_moves.push_back(Move::NameMove::FIRE_BLAST);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::CHARIZARD:
         o_vida = 534;
         o_type = Type::Element::FIRE;
         o_name = "Charizard";
+        o_moves.push_back(Move::NameMove::FLAMETHROWER);
+        o_moves.push_back(Move::NameMove::FIRE_BLAST);
+        o_moves.push_back(Move::NameMove::THRASH);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::ARTICUNO:
         o_vida = 580;
         o_type = Type::Element::ICE;
         o_name = "Articuno";
+        o_moves.push_back(Move::NameMove::AURORA_BEAM);
+        o_moves.push_back(Move::NameMove::BLIZZARD);
+        o_moves.push_back(Move::NameMove::ICE_BEAM);
+        o_moves.push_back(Move::NameMove::SCRATCH);
         break;
     case NomPok::PIDGEY:
         o_vida = 251;
         o_type = Type::Element::WIND;
         o_name = "Pidgey";
+        o_moves.push_back(Move::NameMove::WING_ATTACK);
+        o_moves.push_back(Move::NameMove::PECK);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::PIDGEOT:
         o_vida = 469;
         o_type = Type::Element::WIND;
         o_name = "Pidgeot";
+        o_moves.push_back(Move::NameMove::WING_ATTACK);
+        o_moves.push_back(Move::NameMove::SKY_ATTACK);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::THRASH);
         break;
     case NomPok::BULBASAUR:
         o_vida = 318;
         o_type = Type::Element::EARTH;
         o_name = "Bulbasaur";
+        o_moves.push_back(Move::NameMove::RAZOR_LEAF);
+        o_moves.push_back(Move::NameMove::SOLARBEAM);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::VENUSAUR:
         o_vida = 525;
         o_type = Type::Element::EARTH;
         o_name = "Venusaur";
+        o_moves.push_back(Move::NameMove::RAZOR_LEAF);
+        o_moves.push_back(Move::NameMove::SOLARBEAM);
+        o_moves.push_back(Move::NameMove::ROCK_SLIDE);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::ZAPDOS:
         o_vida = 580;
         o_type = Type::Element::THUNDER;
         o_name = "Zapdos";
+        o_moves.push_back(Move::NameMove::THUNDER);
+        o_moves.push_back(Move::NameMove::THUNDERBOLT);
+        o_moves.push_back(Move::NameMove::WING_ATTACK);
+        o_moves.push_back(Move::NameMove::THRASH);
         break;
     case NomPok::PIKACHU:
         o_vida = 300;
         o_type = Type::Element::THUNDER;
         o_name = "Pikachu";
+        o_moves.push_back(Move::NameMove::THUNDER);
+        o_moves.push_back(Move::NameMove::THUNDERBOLT);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::THRASH);
         break;
     case NomPok::SQUIRTLE:
         o_vida = 314;
         o_type = Type::Element::WATER;
         o_name = "Squirtle";
+        o_moves.push_back(Move::NameMove::BUBBLE);
+        o_moves.push_back(Move::NameMove::WATER_GUN);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::THRASH);
         break;
     case NomPok::BLASTOISE:
         o_vida = 530;
         o_type = Type::Element::WATER;
         o_name = "Blastoise";
+        o_moves.push_back(Move::NameMove::WATERFALL);
+        o_moves.push_back(Move::NameMove::WATER_GUN);
+        o_moves.push_back(Move::NameMove::BUBBLE);
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
         break;
     case NomPok::PORYGON:
         o_vida = 395;
         o_type = Type::Element::NORMAL;
         o_name = "Porygon";
+        o_moves.push_back(Move::NameMove::SKULL_BASH);
+        o_moves.push_back(Move::NameMove::SOLARBEAM);
+        o_moves.push_back(Move::NameMove::SCRATCH);
+        o_moves.push_back(Move::NameMove::THRASH);
         break;
     case NomPok::EEVEE:
         o_vida = 325;
         o_type = Type::Element::NORMAL;
         o_name = "Eevee";
+        o_moves.push_back(Move::NameMove::BUBBLE);
+        o_moves.push_back(Move::NameMove::FIRE_SPIN);
+        o_moves.push_back(Move::NameMove::THUNDER);
+        o_moves.push_back(Move::NameMove::AURORA_BEAM);
         break;
     }
 }
